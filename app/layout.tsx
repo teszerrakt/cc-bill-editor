6e38ae2cba0716f8cc0ddfe2be28ff7bfdc4f0f5
Zaky Syihab Hatmoko <zaky.syihab@gmail.com>
2025-01-28 22:22:09 +0700

    Add link to author in header: include 'by teszerrakt' with hover effect in Billbuddy title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 import { Space_Grotesk } from "next/font/google";
 
@@ -17,7 +18,15 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={`${spaceGrotesk.className} min-h-screen flex flex-col`}>
         <header className="p-4 text-foreground border-b border-foreground">
-          <h1 className="text-2xl font-bold text-center">Billbuddy</h1>
+          <h1 className="text-2xl font-bold text-center">
+            Billbuddy&nbsp;
+            <Link
+              href={"https://zsyihab.com"}
+              className="hover:underline font-thin text-xs"
+            >
+              by teszerrakt
+            </Link>
+          </h1>
         </header>
         {children}
       </body>
